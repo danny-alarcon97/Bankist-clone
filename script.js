089405b45c42ba030af1f64d2c74327a220cb4e8
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2022-07-19 17:23:31 -0400

    login complete

--- script.js
+++ script.js
@@ -85,29 +85,27 @@ const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance}€`;
 };
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
       return int > 1;
     })
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
-calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -139,8 +137,17 @@ btnLogin.addEventListener('click', function (e) {
     }`;
     containerApp.getElementsByClassName.opacity = 100;
 
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
     // Display movements
+    displayMovements(currentAccount.movements);
+
     // Display balance
+    calcDisplayBalance(currentAccount.movements);
+
     // Display summary
+    calcDisplaySummary(currentAccount);
   }
 });
